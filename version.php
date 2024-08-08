2706687acd4afc3cfbae73c7e125e1b5f4562a04
cbadusch <hello@christianbadusch.codes>
2024-08-08 16:50:14 +0200

    Version (2024080800)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024071901;
+$plugin->version   = 2024080800;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [403, 403];
 $plugin->component = 'theme_lexa';
