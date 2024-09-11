bab5344e85b0eec3f862c8d76ea4131cb6124950
winnidat <thomas.winkler@wunderbyte.at>
2024-09-11 12:51:50 +0200

    version 2024091101

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024091100;
+$plugin->version   = 2024091101;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [403, 403];
 $plugin->component = 'theme_lexa';
