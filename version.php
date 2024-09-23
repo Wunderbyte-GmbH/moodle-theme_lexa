6dcd913ef666746cb23a8b247f03e1ccd5844bb7
Georg Maißer <georg.maisser@wunderbyte.at>
2024-09-23 10:09:44 +0200

    Version (2024092300)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024091700;
+$plugin->version   = 2024092300;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [403, 403];
 $plugin->component = 'theme_lexa';
