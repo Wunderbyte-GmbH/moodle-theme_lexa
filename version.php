463860a995c301f93f023a6cdb7699f081f444f3
winnidat <thomas.winkler@wunderbyte.at>
2024-10-01 21:11:36 +0200

    added icons and sccs

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024093001;
+$plugin->version   = 2024093005;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [403, 403];
 $plugin->component = 'theme_lexa';
