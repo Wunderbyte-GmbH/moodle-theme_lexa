70087df430a9717bd417da9ad928ecae2863f709
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-10-17 08:55:08 +0200

    Version 2024101700
    timeline fix

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024101601;
+$plugin->version   = 2024101700;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [403, 403];
 $plugin->component = 'theme_lexa';
