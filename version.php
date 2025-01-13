ee0ca6806de32f715857127b86ef07a508a41194
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-01-13 11:27:39 +0100

    Version 0.1.1 (2025011300)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->release = '0.1.0';
-$plugin->version = 2025011000;
+$plugin->release = '0.1.1';
+$plugin->version = 2025011300;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [404, 404];
 $plugin->component = 'theme_lexa';
