854f0372710f717e384786bb76ec7d761c6ebbdf
Magdalena <78353570+eynimeni@users.noreply.github.com>
2026-05-27 08:36:58 +0200

    Version 0.1.13 (2026052700)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->release = '0.1.12';
-$plugin->version = 2026020400;
+$plugin->release = '0.1.13';
+$plugin->version = 2026052700;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [405, 405];
 $plugin->component = 'theme_lexa';
