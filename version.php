131dee55831faeecdc9c052db7717cf15a5a453b
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2026-06-15 15:19:00 +0200

    Version (2026061500)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->release = '0.1.14';
-$plugin->version = 2026061100;
+$plugin->version = 2026061500;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [405, 405];
 $plugin->component = 'theme_lexa';
