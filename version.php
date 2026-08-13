4d265bacf192eb8378015c736a0f96681de19b3d
cbadusch <cbadusch@users.noreply.github.com>
2026-08-13 12:56:26 +0200

    Version (2026081300)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->release = '0.1.15';
-$plugin->version = 2026080300;
+$plugin->release = '0.1.16';
+$plugin->version = 2026081300;
 $plugin->requires = 2023100900.00;
 $plugin->supported = [405, 405];
 $plugin->component = 'theme_lexa';
